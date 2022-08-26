a8c4cfde7802099957dc90c6b75c274be33d19c0
Vinícius Lavrador <vinicius.lavrador@icloud.com>
2022-08-26 06:14:51 -0300

    chore: linting

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -10,13 +10,13 @@ export const trpc = createReactQueryHooks<AppRouter>();
  * @example type HelloOutput = inferQueryOutput<'hello'>
  */
 export type inferQueryOutput<TRouteKey extends keyof AppRouter["_def"]["queries"]> =
-    inferProcedureOutput<AppRouter["_def"]["queries"][TRouteKey]>;
+  inferProcedureOutput<AppRouter["_def"]["queries"][TRouteKey]>;
 
 export type inferQueryInput<TRouteKey extends keyof AppRouter["_def"]["queries"]> =
-    inferProcedureInput<AppRouter["_def"]["queries"][TRouteKey]>;
+  inferProcedureInput<AppRouter["_def"]["queries"][TRouteKey]>;
 
 export type inferMutationOutput<TRouteKey extends keyof AppRouter["_def"]["mutations"]> =
-    inferProcedureOutput<AppRouter["_def"]["mutations"][TRouteKey]>;
+  inferProcedureOutput<AppRouter["_def"]["mutations"][TRouteKey]>;
 
 export type inferMutationInput<TRouteKey extends keyof AppRouter["_def"]["mutations"]> =
-    inferProcedureInput<AppRouter["_def"]["mutations"][TRouteKey]>;
+  inferProcedureInput<AppRouter["_def"]["mutations"][TRouteKey]>;
